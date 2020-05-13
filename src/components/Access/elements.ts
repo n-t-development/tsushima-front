8a0a205ae207bbaa86c011aa4746232017556bf5
naotoyasaka <not.ysk528@gmail.com>
2020-05-14 00:06:43 +0900

    nonstyle add

--- src/components/Access/elements.ts
+++ src/components/Access/elements.ts
@@ -3,9 +3,11 @@ import styled from "styled-components";
 export const Root = styled.div`
     height: auto;
     margin: 50px 20px;
-    border: solid 1px #808080;
 `;
-
+export const Box = styled.div`
+    margin-top: 20px;
+    border: solid 1px #808080;;
+`;
 export const TitleBox = styled.div`
     width: 100%;
     height: 50%;
--- src/components/Access/index.tsx
+++ src/components/Access/index.tsx
@@ -1,20 +1,27 @@
 import * as React from "react";
-import { Root, TitleBox, Title } from "./elements";
+import { Root, TitleBox, Title, Box } from "./elements";
 
 
 export const Access = () => {
 
     return (
         <Root >
-            <TitleBox>
-                <Title>移動手段</Title>
-            </TitleBox>
-            <p>
-                島外と対馬とを結ぶ交通は空路と海路が選べます。
-                空路は「対馬やまねこ空港」と「福岡空港」「長崎空港」の両空港間で定期便が就航、
-                海路では「厳原」「比田勝」の両港と「福岡（博多港）」「韓国（釜山港）」の間でフェリーや高速船が運行しており、
-                様々な経路で対馬にお越しいただくことができます。
+            <Box>
+                <TitleBox>
+                    <Title>移動手段</Title>
+                </TitleBox>
+                <p>
+                    島外と対馬とを結ぶ交通は空路と海路が選べます。
+                        空路は「対馬やまねこ空港」と「福岡空港」「長崎空港」の両空港間で定期便が就航、
+                        海路では「厳原」「比田勝」の両港と「福岡（博多港）」「韓国（釜山港）」の間でフェリーや高速船が運行しており、
+                        様々な経路で対馬にお越しいただくことができます。
                 </p>
+            </Box>
+            <Box>
+                <TitleBox>
+                    <Title>アクセスの地図画像を作るかも</Title>
+                </TitleBox>
+            </Box>
         </Root>
     )
 }
